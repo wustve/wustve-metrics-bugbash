53e98c73fdda4d37aad658633f572414f1f7729e
Steven Wu <68296503+wustve@users.noreply.github.com>
2026-06-05 13:35:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import http.server
 import socketserver
 
-PORT = 8000  # You can change the port number heres
+PORT = 8000  # You can change the port number heress
 
 # this is another change for another CL
 
